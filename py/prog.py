87be81476b553f744233dcc10f8689caf7c9756f
Simon9877 <46328994+Simon9877@users.noreply.github.com>
2023-01-30 14:44:30 +0100

    Almost v2

--- py/prog.py
+++ py/prog.py
@@ -1,35 +1,33 @@
 #Vi har været lidt stuck og har derfor fået hjælp af Filip. Derfor ligner vores kode måske hans kode. Vi har dog gjordt nogle ting anderledes
 import os, json, random
 import matplotlib.pyplot as plot
-from tkinter import simpledialog
-import colorama
+import tkinter as tk
+import tkinter.font as tkFont
 from colorama import Fore
 
-#Vi importere filen rygsk.json, det her kode er bare for at den faktisk gider at loade.
-current_dir = os.path.dirname(os.path.abspath(__file__))
-print(current_dir)
-os.chdir(current_dir)
+def import_json():
+    #Vi importere filen rygsk.json, det her kode er bare for at den faktisk gider at loade.
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(current_dir)
+    #Vi prøver at loade filen, hvis det ikke lykkes, så printes der en fejlbesked og programmet lukkes.
+    try:
+        f = open('rygsk.json', 'r')
+        indhold = json.load(f)
+        f.close()
+        return indhold
+    except:
+        print(Fore.RED + 'Der er sket en fejl med at loade rygsk.json, tjek at filen er i samme mappe som programmet')
+        exit()
 
-#Vi har ændret rygsk filen til json da den så er nemmere at importere til python
-try:
-    f = open('rygsk.json', 'r')
-    indhold = json.load(f)
-    f.close()
-except:
-    print(Fore.RED + 'Der er sket en fejl med at loade rygsk.json, tjek at filen er i samme mappe som programmet')
-    exit()
-
-
-#her er der nogle variabler som bruges til at bestemme hvor mange individer der skal være i hver generation og hvor mange generationer der skal være
-generationer = int(input(Fore.GREEN + 'Hvor mange generationer vil du have? ' + Fore.WHITE))
-antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? '  + Fore.WHITE))
-if antalIndividerPerGeneration <= 20:
-    print(Fore.RED + 'Der skal være mindst 20 individer i hver generation')
-    while antalIndividerPerGeneration <= 20:
-        antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? ' + Fore.WHITE))
-
-
-
+def start_dialog():
+    #her er der nogle variabler som bruges til at bestemme hvor mange individer der skal være i hver generation og hvor mange generationer der skal være
+    antalGenerationer = int(input(Fore.GREEN + 'Hvor mange generationer vil du have? ' + Fore.WHITE))
+    antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? '  + Fore.WHITE))
+    if antalIndividerPerGeneration <= 20:
+        print(Fore.RED + 'Der skal være mindst 20 individer i hver generation')
+        while antalIndividerPerGeneration <= 20:
+            antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? ' + Fore.WHITE))
+    return antalGenerationer, antalIndividerPerGeneration
 
 # Denne class indeholder alle de ting, som rygsækken skal indeholde. Den indeholder også en metode, som beregner fitness og vægt. Fitness er summen af alle priserne på tingene i rygsækken, og vægten er summen af alle vægtene på tingene i rygsækken. Hvis vægten overstiger 5000, så sættes fitness til 0, da det ikke er muligt at bære en rygsæk med en vægt på over 5000.
 class rygsæk:
@@ -71,42 +69,49 @@ class rygsæk:
             self.fitness = 0
 
 #Laver en population og putter radom ting i rygsækken. Samt beregner fitness for denne rygsæk og tilføjer den til populationen.
-population = []
-total_fitness = 0
-for x in range(antalIndividerPerGeneration):
-    fyldtRygsæk = rygsæk(x)
-    for i in range(5):
-        ting = random.choice(indhold)
-        fyldtRygsæk.tilføj(ting)
-    fyldtRygsæk.beregn_fitness()
-    total_fitness += fyldtRygsæk.fitness
-    population.append(fyldtRygsæk)
-fittest = population[0]
-fit_list = []
+def generer_population(antalIndividerPerGeneration, antalGenerationer):
+    population = []
+    total_fitness = 0
+    for x in range(antalIndividerPerGeneration):
+        fyldtRygsæk = rygsæk(x)
+        for i in range(5):
+            ting = random.choice(indhold)
+            fyldtRygsæk.tilføj(ting)
+        fyldtRygsæk.beregn_fitness()
+        total_fitness += fyldtRygsæk.fitness
+        population.append(fyldtRygsæk)
+    fittest = population[0]
+    fit_list = []
 
+def evolution(popultaion, generationer, fit_list, antalIndividerPerGeneration):
+    for _gen in range(generationer):
+        sorteret = sorted(population, key=lambda rygsæk: rygsæk.fitness, reverse=True)
+        population.clear()
+        #Fjerner de 10% svageste
+        for i in range(int(antalIndividerPerGeneration/10)-1):
+            population.append(sorteret[i])
+        #Laver 10% nye individer
+            for _ in range(int(antalIndividerPerGeneration/30)):
+                nyRygsæk = rygsæk(_gen)
+                nyRygsæk.generer(sorteret[i], sorteret[round(antalIndividerPerGeneration/10)-1-i])
+                nyRygsæk.beregn_fitness()
+                population.append(nyRygsæk)
+        fit_list.append(sorteret[0].fitness)
+        if fittest == sorteret[0]:
+            pass
+        else:
+            fittest = sorteret[0]
+        sorteret.clear()
 
-for _gen in range(generationer):
-    sorteret = sorted(population, key=lambda rygsæk: rygsæk.fitness, reverse=True)
-    population.clear()
-    #Fjerner de 10% svageste
-    for i in range(int(antalIndividerPerGeneration/10)-1):
-        population.append(sorteret[i])
-    #Laver 10% nye individer
-        for _ in range(int(antalIndividerPerGeneration/30)):
-            nyRygsæk = rygsæk(_gen)
-            nyRygsæk.generer(sorteret[i], sorteret[round(antalIndividerPerGeneration/10)-1-i])
-            nyRygsæk.beregn_fitness()
-            population.append(nyRygsæk)
-    fit_list.append(sorteret[0].fitness)
-    if fittest == sorteret[0]:
-        pass
-    else:
-        fittest = sorteret[0]
-    sorteret.clear()
+def tegn_graf(fittest, fit_list):
+    print(f"Total Pris: {fittest.fitness} Total Vægt: {fittest.vaegt} Generation: {fittest.runde}")
+    plot.plot([i for i in range(1, len(fit_list)+1)], fit_list)
+    plot.xlabel("Generation")
+    plot.ylabel("Fitness")
+    plot.show()
 
-30
-print(f"Total Pris: {fittest.fitness} Total Vægt: {fittest.vaegt} Generation: {fittest.runde}")
-plot.plot([i for i in range(1, len(fit_list)+1)], fit_list)
-plot.xlabel("Generation")
-plot.ylabel("Fitness")
-plot.show()
+indhold = import_json()
+antalGenerationer, antalIndividerPerGeneration = start_dialog()
+generer_population()
+evolution()
+tegn_graf()
