c87769626618c6b52dc60621042e49b042c9ed65
Rolf Karmark Andersen <rkarmark@outlook.dk>
2023-01-30 22:56:05 +0100

    Cleanup in files + format python

--- Genetisk-algoritme.py
+++ Genetisk-algoritme.py
@@ -1,36 +1,10 @@
-#Vi har været lidt stuck og har derfor fået hjælp af Filip. Derfor ligner vores kode måske hans kode. Vi har dog gjordt nogle ting anderledes
+#Vi har været lidt stuck og har derfor fået hjælp af Filip. Derfor er små dele af koden fra ham.
 import os, json, random
 import matplotlib.pyplot as plot
-import tkinter as tk
-import tkinter.font as tkFont
 from colorama import Fore
 
-def import_json():
-    #Vi importere filen rygsk.json, det her kode er bare for at den faktisk gider at loade.
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(current_dir)
-    #Vi prøver at loade filen, hvis det ikke lykkes, så printes der en fejlbesked og programmet lukkes.
-    try:
-        f = open('rygsk.json', 'r')
-        indhold = json.load(f)
-        f.close()
-        return indhold
-    except:
-        print(Fore.RED + 'Der er sket en fejl med at loade rygsk.json, tjek at filen er i samme mappe som programmet')
-        exit()
-
-def start_dialog():
-    #her er der nogle variabler som bruges til at bestemme hvor mange individer der skal være i hver generation og hvor mange generationer der skal være
-    antalGenerationer = int(input(Fore.GREEN + 'Hvor mange generationer vil du have? ' + Fore.WHITE))
-    antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? '  + Fore.WHITE))
-    if antalIndividerPerGeneration <= 20:
-        print(Fore.RED + 'Der skal være mindst 20 individer i hver generation')
-        while antalIndividerPerGeneration <= 20:
-            antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? ' + Fore.WHITE))
-    return antalGenerationer, antalIndividerPerGeneration
-
-# Denne class indeholder alle de ting, som rygsækken skal indeholde. Den indeholder også en metode, som beregner fitness og vægt. Fitness er summen af alle priserne på tingene i rygsækken, og vægten er summen af alle vægtene på tingene i rygsækken. Hvis vægten overstiger 5000, så sættes fitness til 0, da det ikke er muligt at bære en rygsæk med en vægt på over 5000.
 class rygsæk:
+    # Denne class indeholder alle de ting, som rygsækken skal indeholde. Den indeholder også en metode, som beregner fitness og vægt. Fitness er summen af alle priserne på tingene i rygsækken, og vægten er summen af alle vægtene på tingene i rygsækken. Hvis vægten overstiger 5000, så sættes fitness til 0, da det ikke er muligt at bære en rygsæk med en vægt på over 5000.
     def __init__(self, gen):
         self.ting = []
         self.fitness = 0
@@ -68,8 +42,32 @@ class rygsæk:
         if self.vaegt > 5000:
             self.fitness = 0
 
+def import_json():
+    #Vi importere filen rygsæk-info.json, det her kode er bare for at den faktisk gider at loade.
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    os.chdir(current_dir)
+    #Vi prøver at loade filen, hvis det ikke lykkes, så printes der en fejlbesked og programmet lukkes.
+    try:
+        f = open('rygsæk-info.json', 'r')
+        indhold = json.load(f)
+        f.close()
+        return indhold
+    except:
+        print(Fore.RED + 'Der er sket en fejl med at loade rygsæk-info.json, tjek at filen er i samme mappe som programmet')
+        exit()
+
+def start_dialog():
+    #her er der nogle variabler som bruges til at bestemme hvor mange individer der skal være i hver generation og hvor mange generationer der skal være
+    antalGenerationer = int(input(Fore.GREEN + 'Hvor mange generationer vil du have? ' + Fore.WHITE))
+    antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? '  + Fore.WHITE))
+    if antalIndividerPerGeneration <= 20:
+        print(Fore.RED + 'Der skal være mindst 20 individer i hver generation')
+        while antalIndividerPerGeneration <= 20:
+            antalIndividerPerGeneration = int(input(Fore.GREEN + 'Hvor mange individer vil du have i hver generation? ' + Fore.WHITE))
+    return antalGenerationer, antalIndividerPerGeneration
+
 #Laver en population og putter radom ting i rygsækken. Samt beregner fitness for denne rygsæk og tilføjer den til populationen.
-def generer_population(antalIndividerPerGeneration, antalGenerationer):
+def generer_population(antalIndividerPerGeneration):
     population = []
     total_fitness = 0
     for x in range(antalIndividerPerGeneration):
@@ -82,8 +80,9 @@ def generer_population(antalIndividerPerGeneration, antalGenerationer):
         population.append(fyldtRygsæk)
     fittest = population[0]
     fit_list = []
+    return fittest, fit_list, population
 
-def evolution(popultaion, generationer, fit_list, antalIndividerPerGeneration):
+def evolution(population, generationer, fit_list, antalIndividerPerGeneration, fittest):
     for _gen in range(generationer):
         sorteret = sorted(population, key=lambda rygsæk: rygsæk.fitness, reverse=True)
         population.clear()
@@ -112,6 +111,6 @@ def tegn_graf(fittest, fit_list):
 
 indhold = import_json()
 antalGenerationer, antalIndividerPerGeneration = start_dialog()
-generer_population()
-evolution()
-tegn_graf()
+fittest, fit_list, population = generer_population(antalIndividerPerGeneration)
+evolution(population, antalGenerationer, fit_list, antalIndividerPerGeneration, fittest)
+tegn_graf(fittest, fit_list)
